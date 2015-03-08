46f587f01043e7be0da41611b45635fd0eaf8a1e
Khushboo <ktekchan@buffalo.edu>
2015-03-08 16:46:48 -0400

    Coded and Compiled execv

--- kern/syscall/execv.c
+++ kern/syscall/execv.c
@@ -1,8 +1,17 @@
 #include <types.h>
 #include <lib.h>
+#include <kern/errno.h>
+#include <kern/fcntl.h>
+#include <thread.h>
+#include <current.h>
+#include <addrspace.h>
+#include <vm.h>
+#include <vfs.h>
+#include <test.h>
 
 #include <kern/execv.h>
 #include <copyinout.h>
+#include <syscall.h>
 
 #define BUFFER_SIZE 255
 
@@ -64,6 +73,71 @@ int sys_execv(const_userptr_t progname, userptr_t args){
 			numBytes += padding;
 		}  		
 	}
+
+	startPoint -= numBytes;
+
+
+	/* Destroy the current address-space */
+	as_destroy(curthread->t_addrspace);
+	curthread->t_addrspace = NULL;
+
+	/* Rest of the functionality is similar to runprogam */
+	struct vnode *v;
+        vaddr_t entrypoint, stackptr;
+
+        /* Open the file. */
+        result = vfs_open(progNameFromUser, O_RDONLY, 0, &v);
+        if (result) {
+                return result;
+        }
+
+        /* We should be a new thread. */
+        KASSERT(curthread->t_addrspace == NULL);
+
+        /* Create a new address space. */
+        curthread->t_addrspace = as_create();
+        if (curthread->t_addrspace==NULL) {
+                vfs_close(v);
+                return ENOMEM;
+        }
+
+        /* Activate it. */
+        as_activate(curthread->t_addrspace);
+
+        /* Load the executable. */
+	        result = load_elf(v, &entrypoint);
+        if (result) {
+                /* thread_exit destroys curthread->t_addrspace */
+                vfs_close(v);
+                return result;
+        }
+
+        /* Done with the file now. */
+        vfs_close(v);
+
+        /* Define the user stack in the address space */
+        result = as_define_stack(curthread->t_addrspace, &stackptr);
+        if (result) {
+                /* thread_exit destroys curthread->t_addrspace */
+                return result;
+        }
+	
+	int stackStart;
+	stackStart = stackptr - numBytes;
+	
+	/* Update the addresses that need to be copied in the user stack*/
+	for (i = 0; i < numArgs; i ++){
+
+		*(int *)startPoint = stackStart + *(int *)startPoint;
+		startPoint += 4;
+	}	                             
+
+	*(int *)startPoint = (int)NULL;
+	startPoint -= (numArgs*4);
+
+	copyout(startPoint, (userptr_t)stackStart, numBytes);
+
+	enter_new_process(numArgs, (userptr_t)stackStart, stackptr, entrypoint);
 	
 	return 0; 
 }
